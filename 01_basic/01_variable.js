b3abdb3e2b1770268e2a914a5f8db4549e09280c
Deepam Bhardwaj <bhardwajdeepam2001@gmail.com>
2024-11-28 18:04:10 +0000

    Day 01

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144335
+let accountEmail = "depamji@google.com"
+var accountPassword = "1234"
+accountCity = "nalagarh" //do not prefer using this way
+let accountState
+
+// accountId = 2
+accountEmail = "bhardwajjii@gmail.com"
+accountPassword= "12345677"
+accountCity = "peersthan"
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState])
+
+/*
+prefer not to use of var variable
+*/
