d0a43aa51388bc4650edccbd7884f1425b15b43d
Deepam Bhardwaj <bhardwajdeepam2001@gmail.com>
2024-11-30 10:23:44 +0000

    day 01 changes

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,8 +1,8 @@
 const accountId = 144335
-let accountEmail = "depamji@google.com"
+let accountEmail = "deepamji@google.com"
 var accountPassword = "1234"
-accountCity = "nalagarh" //do not prefer using this way
-let accountState
+accountCity = "nalagarh" //do not prefer using this way through this way we can just define a space for any data in some memory units
+let accountState // it will give me undefined output/result because there is no data assigned to the variable-account state
 
 // accountId = 2
 accountEmail = "bhardwajjii@gmail.com"
