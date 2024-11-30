71ac1c36bffcbbcfec426de559bb89214aca0a82
Deepam Bhardwaj <bhardwajdeepam2001@gmail.com>
2024-11-30 11:12:55 +0000

    Day 02 Data Types

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict" // treate all code as the newer version of JS
+//alert("hello") //we are using node js not browser 
+
+
+//DATA-TYPE => type of value any variable holds
+//Primitive Data-type
+let name = "Deepam" // string value
+let age = 23 //number value
+let isLoggedIn = true //boolean value
+let state= null  //yeh empty value hai     
+let states=undefined //but undefined data is not empty value it means the value is not defined yet  
+// symbols => unique (will use in react)
+console.table([name,age,isLoggedIn,state])
+
+//Non-Primitive Data-type
+//object
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof state);  //Null is object
+console.log(typeof states);
+
+
